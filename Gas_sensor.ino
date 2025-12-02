da7c88d2736564c863841dc562832d82c0d574c2
krishnaupadhyay00 <krishnabrijwasi00@gmail.com>
2025-12-02 15:45:04 +0530

    Option 1 (short): Initial commit: Add Gas_sensor.ino
    Add Gas_sensor.ino (MQ-series gas sensor example, serial output)
    Option 2 (detailed):
    Add gas sensor example with threshold alert
    
    Add Gas_sensor.ino implementing analog read from MQ sensor, serial logging, and LED/buzzer threshold alert with comments and wiring notes

---
+++ Gas_sensor.ino
@@ -0,0 +1,24 @@
+int sensorPin = A0;       
+int sensorValue = 0;      
+
+void setup() {
+    pinMode(13, OUTPUT);          
+    Serial.begin(9600);          
+}
+
+void loop() {
+    sensorValue = analogRead(sensorPin);
+    if (sensorValue >300) {       
+        digitalWrite(13, LOW); 
+        Serial.print("sensorValue: ");
+        Serial.print(sensorValue);
+        Serial.println(" -> SMOKE DETECTED!");
+    } else {
+        digitalWrite(13, HIGH);    
+        Serial.print("sensor value: ");
+        Serial.print(sensorValue);
+        Serial.println(" -> Normal Air");
+    }
+
+    delay(1000);    
+}
