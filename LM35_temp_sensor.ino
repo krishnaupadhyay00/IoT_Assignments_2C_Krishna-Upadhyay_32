22aca7b2a214da07c78c9236f355770304553f08
krishnaupadhyay00 <krishnabrijwasi00@gmail.com>
2025-12-02 15:57:29 +0530

    Add Arduino sketches: LM35_temp_sensor.ino Add README.md with project overview, wiring notes, and usage Project: IoT_Assignments_2C_Krishna-Upadhyay_32

---
+++ LM35_temp_sensor.ino
@@ -0,0 +1,27 @@
+int sensorPin = A1;  
+float tempC;
+float tempF;
+
+void setup() {
+  Serial.begin(9600);  
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);  
+  
+
+  float voltage = sensorValue * (5.0 / 1023.0);  
+  
+
+  tempC = voltage * 100;  
+  tempF = (tempC * 9.0 / 5.0) + 32.0;  
+
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.print(" °C | ");
+
+  Serial.print(tempF);
+  Serial.println(" °F");  
+
+  delay(1000);  
+}
