6d9c7686c89cd95767001c2f12eadefb31962b23
krishnaupadhyay00 <krishnabrijwasi00@gmail.com>
2025-12-02 15:24:35 +0530

    Initial commit: Add Arduino IoT assignments and README
    - Add Arduino sketches: `OLED.ino`, `Ultrasonic_sensor.ino`, `SratchFile.ino`
    - Add `README.md` with project overview and basic usage
    - Project: IoT_Assignments_2C_Krishna-Upadhyay_32

---
+++ Ultrasonic_sensor.ino
@@ -0,0 +1,28 @@
+int trigPin= 10;
+int echoPin= 9;
+  
+void setup()
+{
+   Serial.begin(9600); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  long duration=pulseIn(echoPin, HIGH);
+  float distance=duration * 0.0343 / 2;
+  
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  delay(500);
+}
